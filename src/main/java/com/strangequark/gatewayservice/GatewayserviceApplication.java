568f6559fa3ad4cf6560b3aa009bf7ca32d0d0a7
StrangeQuark <zacharyzirkle@hotmail.com>
2024-08-28 15:36:58 -0500

    Initial commit, basic route for authservice

---
+++ src/main/java/com/strangequark/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,27 @@
+package com.strangequark.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/auth/**", "/access/**", "/user/**")
+						.filters(f -> f
+								.addResponseHeader("X-Powered-By", "Gateway Service"))
+						.uri("http://localhost:6001")
+				)
+				.build();
+	}
+
+}
