b19f09ff01df881774b1d9f602695e85cad54d5f
StrangeQuark <zacharyzirkle@hotmail.com>
2025-08-10 18:01:55 -0500

    Add logic to handle multiple CORS policies

--- src/main/java/com/strangequark/gatewayservice/config/SecurityConfig.java
+++ src/main/java/com/strangequark/gatewayservice/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.strangequark.gatewayservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
     @Value("${cors.allowed-origins}")
     private List<String> allowedOrigins;
 
@@ -26,5 +30,26 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+    }
+
+    // We must remove and re-add the CORS headers since both Gateway and downstream services must handle CORS policies
+    @Bean
+    public GlobalFilter cleanAndAddCorsHeaders() {
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+
+            // Remove downstream CORS headers first
+            response.getHeaders().remove("Access-Control-Allow-Origin");
+            response.getHeaders().remove("Access-Control-Allow-Credentials");
+            response.getHeaders().remove("Access-Control-Allow-Headers");
+            response.getHeaders().remove("Access-Control-Allow-Methods");
+            response.getHeaders().remove("Access-Control-Expose-Headers");
+
+            // Add gateway CORS headers once
+            response.getHeaders().add("Access-Control-Allow-Origin", allowedOrigins.get(0));
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Headers", "*");
+            response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        }));
     }
 }
